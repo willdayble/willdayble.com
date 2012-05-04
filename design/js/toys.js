6776ce8b2fe76619df33b7ad88ccd8e085498ce4
Will Dayble <will@squareweave.com.au>
2012-05-04 14:19:49 +1000

    LOOK I DID THE WHOLE SITE IN ONE COMMIT AND DIN'T CHECK IT IN OTHER BROWSERS BECAUSE BUGGER OFF

---
+++ design/js/toys.js
@@ -0,0 +1,76 @@
+$(document).ready(function(){
+
+  /* fix up the height on browsers that are too big */
+  $('.js_yelling').css('height', $(window).height());
+  $(window).resize(function() {
+	  $('.js_yelling').css('height', $(window).height());
+	});  
+  
+  $.localScroll();
+  
+  
+  var love = [
+  	{
+  		person : 'Victoria Taylor',
+  		quote : 'is charming.'
+  	},
+  	{
+  		person : 'Victoria Taylor',
+  		quote : 'is disarming.'
+  	},  	
+  	{
+  		person : 'Charlie Gleason',
+  		quote : 'never sleeps.'
+  	},
+  	{
+  		person : 'Michael Wu',
+  		quote : 'has ADHD.'
+  	},
+  	{
+  		person : 'Kirsty Dayman',
+  		quote : 'is batshit mental.'
+  	},
+  	{
+  		person : 'Ben Prendergast',
+  		quote : 'is inspired.'
+  	},
+  	{
+  		person : 'Effie Rose Pryer',
+  		quote : 'is involved.'
+  	},
+  	{
+  		person : 'Michelle Steigerwalt',
+  		quote : 'vomits rainbows.'
+  	},
+  	{
+  		person : 'Lisa Rime',
+  		quote : 'lacks impulse control.'
+  	},
+  	{
+  		person : 'Dane Hunnerup',
+  		quote : 'is a honey badger.'
+  	}
+  ];
+  
+
+  
+	var timer = setInterval( boast, 1000);
+	
+	var counter = 0;
+	
+	function boast() {
+	  if (counter ==0) { counter++; return; }
+	
+		
+		$('.js_quote').text(love[counter].quote);
+		$('.js_person').text(love[counter].person);
+
+		
+	  counter = love.length-1 ? counter = 0 : counter++; 
+	
+	}  
+  
+  
+
+  
+});
