b1759e1e9e58b9cd9d15007459968266f449fca4
Will Dayble <will@squareweave.com.au>
2012-05-04 16:25:08 +1000

    so many little CSS tweaks it makes my teeth itch

--- design/js/toys.js
+++ design/js/toys.js
@@ -3,77 +3,105 @@ $(document).ready(function(){
 	$('.js_get_js').hide();
 
   /* fix up the height on browsers that are too big */
-  $('.js_yelling').css('height', $(window).height());
-  $(window).resize(function() {
+/*
+	if ($('.js_yelling').height() < $(window).height())
+	{
 	  $('.js_yelling').css('height', $(window).height());
-	});  
+	} else
+	{
+		$('.js_yelling').css('height', 'auto');
+	}
+	
+	
+  $(window).resize(function() {
+  	
+  	if ($('.js_yelling').height() < $(window).height())
+  	{
+		  $('.js_yelling').css('height', $(window).height());
+  	} else
+		{
+			$('.js_yelling').css('height', 'auto');
+		}
+	}); 
+*/ 
   
-  $.localScroll();
+  //$.localScroll();
   
   
   var love = [
   	{
-  		person : 'Victoria Taylor',
+  		person : '~ Victoria Taylor',
   		quote : 'is charming.'
   	},
   	{
-  		person : 'Victoria Taylor',
+  		person : '~ Victoria Taylor',
   		quote : 'is disarming.'
   	},  	
   	{
-  		person : 'Charlie Gleason',
+  		person : '~ Charlie Gleason',
   		quote : 'never sleeps.'
   	},
   	{
-  		person : 'Michael Wu',
+  		person : '~ Michael Wu',
   		quote : 'has ADHD.'
   	},
   	{
-  		person : 'Kirsty Dayman',
+  		person : '~ Kirsty Dayman',
   		quote : 'is batshit mental.'
   	},
   	{
-  		person : 'Ben Prendergast',
+  		person : '~ Ben Prendergast',
   		quote : 'is inspired.'
   	},
   	{
-  		person : 'Effie Rose Pryer',
+  		person : '~ Effie Rose Pryer',
   		quote : 'is involved.'
   	},
   	{
-  		person : 'Michelle Steigerwalt',
+  		person : '~ Michelle Steigerwalt',
   		quote : 'vomits rainbows.'
   	},
   	{
-  		person : 'Lisa Rime',
+  		person : '~ Lisa Rime',
   		quote : 'lacks impulse control.'
   	},
   	{
-  		person : 'Dane Hunnerup',
+  		person : '~ Dane Hunnerup',
   		quote : 'is a honey badger.'
   	}
   ];
   
 
   
-	var timer = setInterval( boast, 1000);
-	
 	var counter = 0;
 	
-	function boast() {
-	  if (counter ==0) { counter++; return; }
-	
-		
-		$('.js_quote').text(love[counter].quote);
-		$('.js_person').text(love[counter].person);
+	var timer = setInterval( boast, 1000);
 
+	
+	function boast() {
+
+		if (counter == love.length-1) {counter = 0;}
+		else {counter++;}
 		
-	  counter = love.length-1 ? counter = 0 : counter++; 
+	
+		$('.js_quote').fadeOut(500, function() {
+			$('.js_quote').text(love[counter].quote).fadeIn();  
+	  });
+
+		$('.js_person').fadeOut(500, function() {
+			$('.js_person').text(love[counter].person).fadeIn();  
+	  });
+
+
+		clearInterval(timer);
+		timer = setInterval( boast, 4000);
 	
 	}  
-  
+  boast();
 });
 
+/*
 $(window).bind("load", function() { 
 	$('.js_loading').fadeOut();
 });
+*/
